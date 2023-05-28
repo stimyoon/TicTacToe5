100133043940d9d5404d2d0745e35bb83822f64b
Tim Yoon <stimyoon@gmail.com>
2023-05-28 03:04:34 -0400

    Initial Commit

---
+++ TicTacToe5/TicTacToe5App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe5App.swift
+//  TicTacToe5
+//
+//  Created by Tim Yoon on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
